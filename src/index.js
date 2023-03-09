16839a65028bebfaf9ad15cf4bf3ac8ed65edfdb
nathymunoz <1006106561@u.icesi.edu.co>
2023-03-09 16:47:02 -0500

    cambios en header y seccion 1 + añadir seccion 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import "/src/Components/spotlightZoneVibe100/spotlightZoneVibe100.js";
 import "/src/Components/galleryMasterSeries/galleryMasterSeries.js";
 import "/src/Components/environmentSection/environmentSection.js";
+
+let multipleCardCarousel = document.querySelector(
+    "#pop-carousel"
+  );
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    let carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+    });
+    let carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    let cardWidth = $(".carousel-item").width();
+    let scrollPosition = 0;
+    $("#pop-carousel .carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 4) {
+        scrollPosition += cardWidth;
+        $("#pop-carousel .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $("#pop-carousel .carousel-control-prev").on("click", function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $("#pop-carousel .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
