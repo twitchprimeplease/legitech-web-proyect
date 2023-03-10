81820049bbcadc17749e26c244f345dc5b8504bc
CAT uwu <88683832+CAT9030@users.noreply.github.com>
2023-03-10 02:24:41 -0500

    Seccion 8 y 9 y correcciones de secciones 4y5

--- src/index.js
+++ src/index.js
@@ -36,6 +36,9 @@ let multipleCardCarousel = document.querySelector(
   }
 
   var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+      delay: 4000,
+    },
     pagination: {
       el: ".swiper-pagination",
     },
