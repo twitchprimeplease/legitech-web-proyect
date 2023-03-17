dcb8fe12f2f5b1f8f2df469bd0f638e4b93f7c00
CAT9030 <catagiraldoaguirre@gmail.com>
2023-03-17 08:00:51 -0500

    secciones 8 y9 con autoplay

--- src/index.js
+++ src/index.js
@@ -36,6 +36,9 @@ let multipleCardCarousel = document.querySelector(
   }
 
   var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+      delay: 2500,
+    },
     pagination: {
       el: ".swiper-pagination",
     },
